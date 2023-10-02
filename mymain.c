2e0587d09d74b11f3fb84c40ead409e8d0561c7d
Khloe Wright <khloe.wright@bison.howard.edu>
2023-10-02 14:56:54 +0000

    finishing child process method

--- mymain.c
+++ mymain.c
@@ -2,3 +2,29 @@
 #include  <stdlib.h>
 #include  <string.h>
 #include  <sys/types.h>
+
+void main(void) {
+  pid_t pid;
+
+  pid = fork();
+  if (pid == 0) {
+        ChildProcess();
+  }
+}
+
+void ChildProcess(void) {
+  int i; 
+  int iterations;
+  int time;
+  
+  iterations = rand() % 30;
+  time = rand() % 10;
+
+  for (i = 1; i <= iterations; i++) {
+    printf("Child Pid: %d is doing to sleep!", getpid());
+    sleep(time);
+    printf("Child Pid: is awake!\nWhere is my Parent: %d", getppid());
+  }
+  exit(0);
+}
+
