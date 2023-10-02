d99ce1c8297f1b361e77ed25dcbb12f798791838
Khloe Wright <khloe.wright@bison.howard.edu>
2023-10-02 21:52:16 +0000

    completing lab 2 part 1

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-prog1: main.c
+ prog1: main.c
 	gcc main.c -o prog1
 
 prog2: main2.c
 	gcc main2.c -o prog2
+
+myprog: mymain.c
+	gcc mymain.c -o myprog
--- mymain.c
+++ mymain.c
@@ -1,30 +1,55 @@
+// Author: Khloe Wright
 #include  <stdio.h>
 #include  <stdlib.h>
 #include  <string.h>
 #include  <sys/types.h>
+#include  <sys/wait.h>
+#include  <time.h>
 
-void main(void) {
-  pid_t pid;
+#define MAX 2 
 
-  pid = fork();
-  if (pid == 0) {
-        ChildProcess();
-  }
-}
 
 void ChildProcess(void) {
   int i; 
   int iterations;
   int time;
   
-  iterations = rand() % 30;
-  time = rand() % 10;
+  iterations = rand() % 30 + 1;
+  time = rand() % 10 + 1;
 
   for (i = 1; i <= iterations; i++) {
-    printf("Child Pid: %d is doing to sleep!", getpid());
+    printf("Child Pid: %d is going to sleep!\n", getpid());
     sleep(time);
-    printf("Child Pid: is awake!\nWhere is my Parent: %d", getppid());
+    printf("Child Pid: is awake!\nWhere is my Parent: %d\n", getppid());
   }
   exit(0);
+}
+
+void ParentProcess(void) {
+  int exitstatus; // creating exit statuses for child 1 & 2
+  int child;
+  int k;
+
+  for (k = 0; k < MAX; k++) {
+      child = wait(&exitstatus);
+      printf("Child Pid: %d has completed\n", child);
+  }
+}
+
+void main(void) {
+  pid_t pid;
+  int j;
+  srand(time(NULL));
+
+  for (j = 0; j < MAX; j++ ) {
+      pid = fork();
+      if (pid == 0) {
+        ChildProcess();
+      } else if (pid < 0) {
+        printf("Error: Could not fork child processes.\n");
+        exit(0);
+      }
+  }
+  ParentProcess();
 }
 
